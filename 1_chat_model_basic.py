9c649ff280bacb983223bcb175ecdab11b532a35
Akshay Ramesh Bhivagade <115061663+akshayrbhm@users.noreply.github.com>
2024-07-17 20:09:20 +0530

    Add files via upload

---
+++ 1_chat_model_basic.py
@@ -0,0 +1,19 @@
+# Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/
+# OpenAI Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/openai/
+
+from dotenv import load_dotenv
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# Load environment variables from .env
+load_dotenv()
+
+# Create a model
+model = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+
+# Invoke the model with a message
+result = model.invoke("What is 81 divided by 9?")
+print("Full result:")
+print(result)
+print("Content only:")
+print(result.content)
